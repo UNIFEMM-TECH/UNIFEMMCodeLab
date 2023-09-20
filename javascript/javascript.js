3d1137ee4a97554cd85ae0b8ec45a5e16fb29221
UNIFEMM-TECH <uni-tech@unifemm.edu.br>
2023-09-20 19:58:46 -0300

    organizando códigos

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,35 +1,33 @@
+// Função para mostrar uma seção e armazenar o ID no localStorage
+function mostrarConteudo(id) {
+    // Esconde todos os elementos de conteúdo
+    const elementosConteudo = document.querySelectorAll('.container section');
+    elementosConteudo.forEach(elemento => {
+        elemento.style.display = 'none';
+    });
 
-        
-        // Função para mostrar uma seção e armazenar o ID no localStorage
-        function mostrarConteudo(id) {
-            // Esconde todos os elementos de conteúdo
-            const elementosConteudo = document.querySelectorAll('.container section');
-            elementosConteudo.forEach(elemento => {
-                elemento.style.display = 'none';
-            });
+    // Mostra o elemento com o ID correspondente
+    const elemento = document.getElementById(id);
+    if (elemento) {
+        elemento.style.display = 'block';
 
-            // Mostra o elemento com o ID correspondente
-            const elemento = document.getElementById(id);
-            if (elemento) {
-                elemento.style.display = 'block';
+        // Armazena o ID da seção ativa no localStorage
+        localStorage.setItem('secaoAtiva', id);
+    }
+}
 
-                // Armazena o ID da seção ativa no localStorage
-                localStorage.setItem('secaoAtiva', id);
-            }
-        }
+// Recupera o ID da última seção ativa do localStorage e a exibe
+const secaoAtiva = localStorage.getItem('secaoAtiva');
+if (secaoAtiva) {
+    mostrarConteudo(secaoAtiva);
+}
 
-        // Recupera o ID da última seção ativa do localStorage e a exibe
-        const secaoAtiva = localStorage.getItem('secaoAtiva');
-        if (secaoAtiva) {
-            mostrarConteudo(secaoAtiva);
-        }
+const elementosConteudo = document.querySelectorAll('.container section');
+elementosConteudo.forEach(elemento => {
+    elemento.style.display = 'none';
+});
 
-        const elementosConteudo = document.querySelectorAll('.container section');
-            elementosConteudo.forEach(elemento => {
-                elemento.style.display = 'none';
-            });
-
-        const elemento = document.getElementById('inicio');
-            if (elemento) {
-                elemento.style.display = 'block';
-            }
+const elemento = document.getElementById('inicio');
+if (elemento) {
+    elemento.style.display = 'block';
+}
