3d349c6e01934d8c3be15e9f4df5ccd2cb99c880
UNIFEMM-TECH <uni-tech@unifemm.edu.br>
2023-10-03 09:19:48 -0300

    atualização imagens cartao gald silver brone diamond

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -27,7 +27,7 @@ elementosConteudo.forEach(elemento => {
     elemento.style.display = 'none';
 });
 
-const elemento = document.getElementById('colaboradores');
+const elemento = document.getElementById('inicio');
 if (elemento) {
     elemento.style.display = 'block';
 }
